0e5639aa2d8ad7f7890fe52838bdc4b365f16a07
Bruno S. Brasil <bruh-sb@live.com>
2020-02-13 16:20:16 -0300

    feat(terraform): Reorganize providers versions

--- infra - GCP/main.tf
+++ infra - GCP/main.tf
@@ -3,6 +3,13 @@ data "google_client_config" "default" {}
 
 terraform { #TODO - Create Docs
   required_version = ">= 0.12"
+  required_providers {
+
+    google-beta = "~> 3.7.0" # https://github.com/terraform-providers/terraform-provider-google/releases
+    google      = "~> 3.7.0" # https://github.com/terraform-providers/terraform-provider-google-beta/releases
+    kubernetes  = "~> 1.10"  # https://github.com/terraform-providers/terraform-provider-kubernetes/releases
+
+  }
 
   backend "gcs" {
     bucket = "terraform-state-labs-k8s"
@@ -11,22 +18,23 @@ terraform { #TODO - Create Docs
 }
 
 provider "google-beta" {
+
   credentials = var.gcp_credentials_json
   project     = var.gcp_project_id
   region      = var.region
   zone        = var.zone
-  version     = "~> 3.7.0"
 }
 
 provider "google" {
+
   credentials = var.gcp_credentials_json
   project     = var.gcp_project_id
   region      = var.region
   zone        = var.zone
-  version     = "~> 3.7.0"
 }
 
 provider "kubernetes" { #TODO - Create Docs
+
   load_config_file       = false
   host                   = "https://${module.kubernetes.endpoint}"
   cluster_ca_certificate = base64decode(module.kubernetes.ca_certificate)
